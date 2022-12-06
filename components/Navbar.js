d8abdce2d3ead51a23f094897b3cde44a196d803
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-12-06 19:40:20 -0300

    refactor: :recycle: Adição de uma Section para facilitar a estilização

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,29 +5,31 @@ import styles from '../styles/Navbar.module.css';
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div>
-        <Image
-          src='/images/pokeball.png'
-          width='30'
-          height='30'
-          alt='pokeball image'
-        />
-        <h1>
-          Pokedex
-        </h1>
-      </div>
-      <ul>
-        <li>
-          <Link href='/'>
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link href='/about'>
-            About
-          </Link>
-        </li>
-      </ul>
+      <section>
+        <div>
+          <Image
+            src='/images/pokeball.png'
+            width='30'
+            height='30'
+            alt='pokeball image'
+          />
+          <h1>
+            Pokedex
+          </h1>
+        </div>
+        <ul>
+          <li>
+            <Link href='/'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href='/about'>
+              About
+            </Link>
+          </li>
+        </ul>
+      </section>
     </nav>
   )
 }
