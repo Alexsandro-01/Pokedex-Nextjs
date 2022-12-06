5f365952f20158e3412dfa4a53c469e179f19299
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-12-06 15:28:23 -0300

    feat: :sparkles: Componentes de Navbar e de Footer da aplicação

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Footer.module.css'
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>Pokedex</span> &copy; 2022
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from '../styles/Navbar.module.css';
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div>
+        <Image
+          src='/images/pokeball.png'
+          width='30'
+          height='30'
+          alt='pokeball image'
+        />
+        <h1>
+          Pokedex
+        </h1>
+      </div>
+      <ul>
+        <li>
+          <Link href='/'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
