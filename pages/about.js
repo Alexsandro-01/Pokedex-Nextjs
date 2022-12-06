b33fd10bbbaf1c7a451bbf3250473c00d489f9d9
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-12-06 19:39:38 -0300

    fix: :pencil2: Correção de gramática no texto

--- pages/about.js
+++ pages/about.js
@@ -9,7 +9,7 @@ function About() {
         Sobre o projeto
       </h1>
       <p>
-        Este projeto foi desenvolvido enquanto acompanhava o curso de Next.js no <Link href="https://www.youtube.com/playlist?list=PLnDvRpP8BnezfJcfiClWskFOLODeqI_Ft" target="_blank">youtube</Link>, ministrado pelo Matheus Battisti, para praticar os aprendizados de Next.js. A Pokeapi foi uma excelente e divertida forma para dar os primeiros passos com o Next e começar a entender como Server Side Rendering funciona, como o roteamento das páginas finciona no Next entre  outras coisas. Apartir de agora vou dar os próximos passos para me aprofundar ainda mais na usabilidade, vantagens e especificidades do Next.js.
+        Este projeto foi desenvolvido enquanto acompanhava o curso de Next.js no <Link href="https://www.youtube.com/playlist?list=PLnDvRpP8BnezfJcfiClWskFOLODeqI_Ft" target="_blank">youtube</Link>, ministrado pelo Matheus Battisti, para praticar os aprendizados de Next.js. A Pokeapi foi uma excelente e divertida forma para dar os primeiros passos com o Next e começar a entender como Server Side Rendering funciona, como o roteamento das páginas funciona no Next entre  outras coisas. Apartir de agora vou dar os próximos passos para me aprofundar ainda mais na usabilidade, vantagens e especificidades do Next.js.
       </p>
       <Image
         src='/images/charizard.png'
